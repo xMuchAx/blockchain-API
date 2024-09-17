dc215ec09e312eb2f6ce0a6a53638997dcd1bf8c
jacin <jacinto.valentino27@gmail.com>
2024-09-17 16:38:44 +0200

    deployement

--- API/routes/users.js
+++ API/routes/users.js
@@ -1,13 +1,13 @@
 import express from "express";
 const router = express.Router();
 import {getCatTokenQuantity} from "../../contract_service/cat_contract.js"
-const LOCAL_GANACHE_SERVER = "http://localhost:7545";
+const POLYGON_SERVER = "https://rpc-amoy.polygon.technology/";
 const CAT_TOKEN_0 = 0;
 
 //route to get all user info (ca recup que les token pour l'instant)
 router.get("/:tokenAccount", async (req, res) => {
   try {
-      const AccountTokenQty =   await getCatTokenQuantity(LOCAL_GANACHE_SERVER, req.params.tokenAccount, CAT_TOKEN_0);
+      const AccountTokenQty =   await getCatTokenQuantity(POLYGON_SERVER, req.params.tokenAccount, CAT_TOKEN_0);
       
       res.status(200).json({
           Account: `this account contain: ${AccountTokenQty}`,
