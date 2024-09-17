89eefb4d0ecbaf90e8b8aae20eeeea6a8751f84e
jacin <jacinto.valentino27@gmail.com>
2024-09-17 16:55:52 +0200

    test 2

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-require("@nomicfoundation/hardhat-ignition-ethers");
+import "@nomicfoundation/hardhat-ignition-ethers";
 const POLYGON_SERVER = "https://rpc-amoy.polygon.technology/";
  const MY_PRIVATE_KEY =
   "f42dff05a6dd9ccc9b19466418703f9b1e0480dd0499efa75a3e2c790cb17d70";
