21e7e63275dc0b8c3af100e0fdb9379eec8926b8
diallo alpha <dalpha16@gmail.com>
2024-09-18 15:42:22 +0200

    feet:modification route register

--- API/index.js
+++ API/index.js
@@ -2,13 +2,13 @@ import express from "express";
 const port = process.env.PORT || 3000;
 export const contract_adress = process.env.CONTRACT_ADRESS;
 const app = express();
-import users from "./routes/users.js";
+import authentification from "./routes/authentification.js";
 
 app.get("/", (req, res) => {
   res.send("Hello World222hhh2!");
 });
 
-app.use("/users", users);
+app.use("/authentification", authentification);
 
 app.listen(port, () => {
   console.log(`Serveur is online on port ${port}`);
