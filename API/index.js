fecba6e268efe6ced3eaeed369defff8bc15aec0
Erwann QUEVIT <quevit.erwann@mail-ecv.fr>
2024-09-19 22:59:48 +0200

    add swagger

--- API/index.js
+++ API/index.js
@@ -1,20 +1,44 @@
 import express from "express";
 const port = process.env.PORT || 3000;
 export const contract_adress = process.env.CONTRACT_ADRESS || "0x3111BaeECB3a4465D3Ba0Cae67097fC326Cb5860"
-const app = express();
-import authentification from "./routes/authentification.js"; 
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+import authentification from "./routes/authentification.js";
 import token from "./routes/token.js"
 
 import cors from "cors"
 import bodyParser from "body-parser"
 
-app.options('*', cors()) 
+const app = express();
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      version: "1.0.0",
+      title: "Cat API",
+      description: "API pour le projet Cat",
+      contact: {
+        name: "Cat API"
+      },
+      servers: ["http://localhost:3000"]
+    }
+  },
+  apis: ["./API/routes/*.js"]
+};
+
+const specs = swaggerJsdoc(swaggerOptions);
+
+app.options('*', cors())
 
 app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use("/authentification", authentification)
 
